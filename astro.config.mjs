ef0e890a3e7479cfd7d843f76cebf32dc30936fd
Utkuu0 <muhammedutkuaksu1@gmail.com>
2026-01-03 23:45:05 +0300

    fix astro config for gh-pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://utkuaksu.com',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
